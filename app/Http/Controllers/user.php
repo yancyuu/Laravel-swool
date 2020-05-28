abd76f6b4c6da8d57fd23a17b1f928a795654777
yancy <1191500820@qq.com>
2020-05-28 18:49:31 +0800

    test post

---
+++ app/Http/Controllers/user.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+class user extends BaseController
+{
+    public function test(Request  $request){
+        $post = $request->all();
+        return $post;
+    }
+
+}
