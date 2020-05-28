3e559823545346a39a9dda0d02fdf08395bcd976
yancy <1191500820@qq.com>
2020-05-28 19:53:03 +0800

    test post

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/UserTest',function (){
-    return App\Http\
-});
+Route::post('/Test','Controller@todo');
