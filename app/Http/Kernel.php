07ee1ae33ab61de65408371501ad719aec193244
yancy <1191500820@qq.com>
2020-05-29 19:08:28 +0800

    App\Http\Kernal 中注册中间件

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'api_auth' => Qbhy\LaravelApiAuth\LaravelApiAuthMiddleware::class,
+        'api_auth' => \Qbhy\LaravelApiAuth\Middleware::class,
     ];
 }
